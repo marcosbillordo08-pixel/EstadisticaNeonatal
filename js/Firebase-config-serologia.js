1ee486cf28fa08cb70843bf3798c2e55e7f69cac
marcosbillordo08-pixel <marcosbillordo08@gmail.com>
2026-07-03 23:44:36 -0300

    Update Firebase-config-serologia.js

--- js/Firebase-config-serologia.js
+++ js/Firebase-config-serologia.js
@@ -1,13 +1,12 @@
 const firebaseConfig = {
-            apiKey: "AIzaSyC21cJIHv1sEBQ2WVgBB_b3OG8rklNhJmg",
-            authDomain: "stocklab-4e30e.firebaseapp.com",
-            projectId: "stocklab-4e30e",
-            storageBucket: "stocklab-4e30e.firebasestorage.app",
-            messagingSenderId: "104155908756",
-            appId: "1:104155908756:web:314cd3dc78ef94437a0281",
-            measurementId: "G-PVKEDY9JY9"
-        };
+    apiKey: "TU_API_KEY_NUEVA",
+    authDomain: "estadisticaneonatal-xxxx.firebaseapp.com",
+    projectId: "estadisticaneonatal-xxxx",
+    storageBucket: "estadisticaneonatal-xxxx.firebasestorage.app",
+    messagingSenderId: "...",
+    appId: "..."
+};
 
-        firebase.initializeApp(firebaseConfig);
-        const auth = firebase.auth();
-        const db = firebase.firestore();
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
