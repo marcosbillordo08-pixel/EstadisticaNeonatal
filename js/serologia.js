86ded2a09d05c7488ca64265d96bb89afb509a86
marcosbillordo08-pixel <marcosbillordo08@gmail.com>
2026-07-28 16:19:33 -0300

    Update serologia.js

--- js/serologia.js
+++ js/serologia.js
@@ -123,6 +123,14 @@ function formatearFechaCorta(fecha) {
     return `${dia}/${mes}`;
 }
 
+funtion formatearFechaCompleta(fecha) {
+    const dia = String(fecha.getDate()).padStart(2, "0");
+    const mes = String(fecha.getMonth() + 1).padStart(2, "0");
+    const anio = fecha.getFullYear();
+
+   return `${dia}/${mes}/${anio}`;
+}
+
 function fechaHoyTexto() {
     return formatearFechaCompleta(new Date());
 }
